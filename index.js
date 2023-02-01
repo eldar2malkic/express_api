668763b6bc69f8b3cf88bd2779caf921813f4ffc
eldar2malkic <88284877+eldar2malkic@users.noreply.github.com>
2023-01-31 22:07:53 -0600

    final commit

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const app = express();
 
+
+const courses = [
+  {id: 1, name: 'course1' },
+  {id: 2, name: 'course2' },
+  {id: 3, name: 'course3' },
+];
+
 app.get('/', (req, res) => {
-    res.send('Github is coolest');
+    res.send('Github is coolest!');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
  app.get('/api/courses/:id', (req, res) => {
-   res.send(req.params.id);
- })  ;
+   const course = courses.find(c => c.id === parseInt(req.params.id));
+   if (!course) res.status(404);
+   res.send(course);
+ });
 
 // PORT
 const port = process.env.PORT || 3000;
